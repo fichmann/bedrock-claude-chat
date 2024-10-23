cc1f0be4606a765cc606c2ca2c66c792bb4d10f5
Renato Fichmann <fichmann@amazon.com>
2024-10-23 11:09:02 -0400

    Traducoes

--- frontend/src/i18n/en/index.ts
+++ frontend/src/i18n/en/index.ts
@@ -6,8 +6,8 @@ const translation = {
       },
     },
     app: {
-      name: 'Bedrock Claude Chat',
-      nameWithoutClaude: 'Bedrock Chat',
+      name: 'AI Silvia Fichmann',
+      nameWithoutClaude: 'AI - Silvia Fichmann',
       inputMessage: 'Send a message',
       starredBots: 'Starred Bots',
       recentlyUsedBots: 'Recently Used Bots',
